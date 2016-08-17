43f727a496c520dbbe1a61bb2279833e09c4db3d
Nikhil Apte <Nikhil.Apte@veritas.com>
2016-08-17 18:50:06 +0530

    Publish debug event on health check

--- event-service/src/main/java/com/naptex/evtsvc/rest/HealthCheckResource.java
+++ event-service/src/main/java/com/naptex/evtsvc/rest/HealthCheckResource.java
@@ -2,6 +2,7 @@ package com.naptex.evtsvc.rest;
 
 import java.util.Date;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +12,10 @@ import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.naptex.evtsvc.model.Event;
+import com.naptex.evtsvc.model.Level;
+import com.naptex.evtsvc.service.EventProcessorService;
+
 @Path("/health")
 @Component
 @Scope("request")
@@ -18,10 +23,25 @@ public class HealthCheckResource
 {
 	final static Logger logger = Logger.getLogger(HealthCheckResource.class);
 
+	private final EventProcessorService service;
+
+	@Inject
+	public HealthCheckResource(EventProcessorService service)
+	{
+		this.service = service;
+	}
+
 	@GET
 	@Produces("text/html")
 	public Response getHealthCheckPage()
 	{
+		Event healthCheckEvent = new Event();
+		healthCheckEvent.setLevel(Level.DEBUG);
+		healthCheckEvent.setMessage("Health Check");
+		healthCheckEvent.setOrigin("SYS");
+		healthCheckEvent.setTime(new Date());
+		service.publishEvent(healthCheckEvent);
+
 		String output = "<h1>Health Check</h1>"
 				+ "<p>RESTful Service is running ... <br>Ping @ "
 				+ new Date().toString() + "</p><br>";
